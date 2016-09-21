615e880269ffa2a447476bcc4f47062a0174e657
MEDARD Thomas <m14002147@a-f000727-l.salsa.univ-amu.fr>
2016-09-21 17:54:12 +0200

    trucs

---
+++ src/Sorts/DichoInser.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+/*
+* Sort an int tab with dochotomous/permutation algorithim.
+* @param tab The table of int to sort.
+* @param tabSize the size of tab.
+* @author Thomas MEDARD
+*/
+void dichoInser (int tab[], const size_t tabSize) {
+	int i, j, left, right, middle, save;
+
+	for (i = 1; i < tabSize; ++i) {
+
+        left = 0;
+        right = i;
+
+		while (left < right) {
+            middle = (left + right) / 2;
+            
+            if (tab[i] <= tab[middle]) {
+                right = middle;
+            }
+            else {
+                left = middle + 1;
+            }
+        }
+
+		save = tab[i];
+        for (j = i; j > left; --j) {
+            tab[j] = tab[j - 1];
+        }
+		tab[left] = save;
+	}
+}
---
+++ src/Sorts/SelecPerm.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+/*
+* Sort an int tab with selection/permutation algorithim.
+* @param tab The table of int to sort.
+* @param tabSize the size of tab.
+* @author Thomas MEDARD
+*/
+void selecPerm (int tab[], const size_t tabSize) {
+	size_t i, j;
+    int min, save;
+
+	for (i = 0; i < tabSize - 1; ++i) {
+		j = i + 1;
+		min = i;
+
+		for (j = i + 1; j < tabSize; ++j) {
+			if (tab[j] < tab[min]) {
+				min = j;
+			}
+		}
+
+		save = tab[i];
+		tab[i] = tab[min];
+		tab[min] = save;
+	}
+}
