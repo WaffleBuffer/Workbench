d1bae324fe63abfb3540edf832a512b5605b0967
WaffleBuffer <dark1wador@ymail.com>
2016-09-29 09:23:53 +0200

    debut vrai objet

--- src/list.c
+++ src/list.c
@@ -2,10 +2,113 @@
 
 #include <stdlib.h>
 
-void new_NODE (NODE* This) {
-	This = (NODE) malloc(sizeof(NODE));
+static void TPile_Init(TPile *This)
+{
+       This->Push = TPile_Push;
+       This->Pop = TPile_Pop;
+       This->Clear = TPile_Clear;
+       This->Length = TPile_Length;
+       This->View = TPile_View;
+       This->Nombre = 0;
+       This->Top = NULL;
 }
 
-void free_NODE(NODE* This) {
-	free(This);
+int List_push(List *This, const TYPE val)
+{
+	Node *newNode = malloc(sizeof(Node));
+	
+	if(!newNode) {
+		return NODE_ALLOC_ERROR;
+	}
+	
+	newNode->value = val;
+	newNode->next = This->top;
+	This->top = newNode;
+	This->++size;
+	
+	return 0;
+}
+
+int TPile_Pop(TPile *This)
+{
+        int Val;
+        Titem *tmp;
+        if(!This->Top) return PILE_EMPTY;
+        tmp = This->Top->prec;
+        Val = This->Top->Value;
+        free(This->Top);
+        This->Top = tmp;
+        This->Nombre--;
+        return Val;
+}
+/******************************************************************************/
+ 
+void TPile_Clear(TPile *This)
+{
+        Titem *tmp;
+        while(This->Top)
+          {
+             tmp = This->Top->prec;
+             free(This->Top);
+             This->Top = tmp;
+          }
+        This->Nombre = 0;
+}
+/******************************************************************************/
+ 
+int TPile_Length(TPile *This)
+{
+        return This->Nombre;
+}
+/******************************************************************************/
+ 
+void TPile_View(TPile *This)
+{
+       Titem *tmp = This->Top;
+       while(tmp)
+          {
+             printf("%d\n",tmp->Value);
+             tmp = (*tmp).prec;
+          }
+}
+
+static void List_init(List *This)
+{
+	This->size = 0;
+	This->top = NULL; 
+	This->push = List_push;
+	This->pop = List_pop;
+	This->clear = List_clear;
+	This->length = List_length;
+	This->view = List_view;      
+}
+
+void List_free(TPile *This)
+{
+	List_clear(This);
+	puts("Destruction de la pile statique.\n");
+}
+
+void List_newFree(TPile *This)
+{
+	if(This) List_clear(This);
+	free(This);        
+	puts("Destruction de la pile dynamique.\n");
+}
+
+List List_create()
+{
+	List This;
+	List_Init(&This);
+	List.Free = List_free;
+	return This;
+}
+
+List* new_List()
+{
+	List *This = malloc(sizeof(List));
+	if(!This) return NULL;
+	List_Init(This);
+	This->Free = List_newFree;
+	return This;
 }
--- src/list.h
+++ src/list.h
@@ -1,15 +1,49 @@
+#include <
+
 #ifndef LIST__H
 #define LIST__H
+
+#define NODE_ALLOC_ERROR 1
+#define PILE_EMPTY      -1
+
+#ifdef __cplusplus
+	extern "C" {
+#endif
 
 // The type of the list
 typedef int TYPE;
 
-typedef struct node{
+typedef struct Node {
 	TYPE value;
-	struct node* next;	
+	struct Node *prec;
+} Node;
+
+typedef struct List {
+	size_t size;
+	struct Node *top;	
 	
-	void (*new_LIST);
-	void (*free);
-}NODE, *LIST;
+	int(*push)(struct List*, const TYPE);
+	TYPE(*pop)(struct List*);
+	void(*clear)(struct List*);
+	void(*free)(struct List*);
+	int(*length)(struct List*);
+	void(*view)(struct List*);
+}List;
+
+List List_create(void);
+List* new_List(void);
+
+int List_push(List*, const TYPE);
+TYPE List_Pop(List*);
+void List_clear(List*); 
+int List_length(List*);
+void List_view(List*);
+
+void List_free(List*);
+void List_newFree(List*);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* LIST__H */
