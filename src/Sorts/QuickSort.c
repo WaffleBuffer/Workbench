b5658879340ae1991495780bd84e1df2876ee52a
WaffleBuffer <dark1wador@ymail.com>
2016-10-12 14:08:06 +0200

    Yeah!

--- src/Sorts/QuickSort.c
+++ src/Sorts/QuickSort.c
@@ -2,40 +2,27 @@
 
 #include <stdlib.h>
 
-/*void swap(TYPE tab[], TYPE x, TYPE y) {
+#ifndef QUICKSORT__c
+#define QUICKSORT__c
+
+void swap(TYPE tab[], TYPE x, TYPE y) {
 	TYPE tmp = tab[x];
 	tab[x] = tab[y];
 	tab[y] = tmp;
-}*/
+}
 
 void putPivot (TYPE tab[], const size_t left, const size_t right, size_t *p) {
 	*p = left;
-	TYPE tmp;
-	
-	//printf("left : %zu right : %zu\n", left, right);
+		
 	for (size_t i = left + 1; i <= right; ++i) {
-		//printf("%zu : %zu\n", i, right);
 		if (tab[i] < tab[left]) {
 			
-			
 			++(*p);
-			//printf("debug1.0\n");
-			tmp = tab[i];
-			//printf("debug1.1\n");
-			tab[i] = tab[*p];
-			//printf("debug1.2\n");
-			tab[*p] = tmp;
-			//printf("debug1.3\n");
+			swap(tab, i, *p);
 		}
 	}
 	
-	//printf("Après\n");
-	tmp = tab[left];
-	tab[left] = tab[*p];
-	tab[*p] = tmp;
-	
-	//printf("fin\n");
-	//swap (tab, left, *p);
+	swap(tab, left, *p);
 }
 
 void quickSort (TYPE tab[], const size_t left, const size_t right) {
@@ -52,3 +39,5 @@ void quickSort (TYPE tab[], const size_t left, const size_t right) {
 void quickSortMain (TYPE tab[], const size_t tabSize) {
 	quickSort( tab, 0, tabSize -1);
 }
+
+#endif /* QUICKSORT__c */
--- src/Workbench.c
+++ src/Workbench.c
@@ -22,7 +22,7 @@
 #include <signal.h> //SA_RESTART
 
 // Table sizes to test
-#define SIZE1  10
+#define SIZE1  100
 #define SIZE2  500
 #define SIZE3  5000
 #define SIZE4  10000
@@ -53,10 +53,10 @@
 #define NB_TEST_PER_SIZE 20
 
 // Number of test to do
-#define NB_SIZE_TO_TEST 1
+#define NB_SIZE_TO_TEST 15
 
 // Maximum time allowed for a size test (5 minutes)
-#define TIME_BEFORE_STOP 10
+#define TIME_BEFORE_STOP 300
 
 // Maximum value for a random element
 #define MAX_RAND_VALUE 100
@@ -276,10 +276,10 @@ void launchTest(const size_t sizeToTest) {
 		// Generating  its values
 		initTabRand(tab, sizeToTest);
 		
-		for (size_t i = 0; i < sizeToTest; ++i) {
+		/*for (size_t i = 0; i < sizeToTest; ++i) {
 			printf("%d ", tab[i]);
 		}
-		printf("\n");
+		printf("\n");*/
 		
 		// Begin time mesure
 		clock_t begin = clock();
@@ -290,10 +290,10 @@ void launchTest(const size_t sizeToTest) {
 		// End time mesure
 		clock_t end = clock();
 		
-		for (size_t i = 0; i < sizeToTest; ++i) {
+		/*for (size_t i = 0; i < sizeToTest; ++i) {
 			printf("%d ", tab[i]);
 		}
-		printf("\n\n");
+		printf("\n\n");*/
 
 		// Calculating passed time
 		resultsSum += ((double)end - (double)begin) / CLOCKS_PER_SEC * 1000.0;
